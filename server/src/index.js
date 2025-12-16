91db5b599c3f2fe50700de035947c169b0887079
Divyanshu <dnd.mein@gmail.com>
2025-12-16 16:54:17 +0530

    add Prisma ORM setup with PostgreSQL database integration

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,12 @@
-import express from 'express';
-import "dotenv/config";
+import express from "express";
+import "dotenv/config"
 
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.send("Hello World");
+    res.send('Hllo');
 })
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port http://localhost:${process.env.PORT}`);
 })
-
-export default app;
